f27c555d59c6b265ae2945ae9c760311c06c081d
Fedya <you@example.com>
2019-07-09 15:42:18 +0700

    new sprite types: 1 2 4 frames per animation

--- project/sprite.lua
+++ project/sprite.lua
@@ -1,10 +1,7 @@
 -- Библиотека загрузки и рисования спрайтов
 
-local framesOrderMobs = { 2, 1, 2, 3 }
-local framesOrderBurn = { 1, 2 }
-local framesOrderItem = { 1 }
-
-function newSpr(name, width, height, delay, view, anima)
+function newSpr(name, width, height, delay, view, anima, framesOrder)
+	-- view- (ракурс 1 или 4), anima - кол-во фреймов в анимации
     local t = {
         name = name,
         width = width,
@@ -15,39 +12,33 @@ function newSpr(name, width, height, delay, view, anima)
         viewSprite = { "Down", "Up", "Left", "Right" },
         frameDelay = delay or 0.5, --КОНСТАНТА через сколько переключаться на новый кадр
         curDelay = 0, -- сколько осталось времени до переключения на новый кадр анимации
+		framesOrder = framesOrder or {1}
     }
-
     for i = 1, view do
         for j = 1, anima do
-            local f = t.viewSprite[i] .. j
+			if view == 1 then
+				i = nil
+			end
+			if anima == 1 then
+				j = nil
+			end
+            --local f = t.viewSprite[i] .. j
             --print(name..t.viewSprite[i]..j..".png")
-            t.spr[f] = love.graphics.newImage(name .. t.viewSprite[i] .. j .. ".png")
-        end
-    end
-    return t
-end
-
---[[function newSpr2(name, width, height)
-	local t2 = {
-		name = name,
-		width = width,
-		height = height,
-		spr = {},
-		curAnim = "Down",
-		curFrame = 1,
-		viewSprite = {"Up", "Down", "Left", "Right"},
-		frameDelay = 0.2, --КОНСТАНТА через сколько переключаться на новый кадр
-		curDelay = 0, -- сколько осталось времени до переключения на новый кадр анимации
-	}
-	
-	for i = 1, 4 do
-		for j = 1, 3 do
-			local f = t2.viewSprite[i]..j
-			t2.spr[f] = love.graphics.newImage(name..t2.viewSprite[i]..j..".png")
+			if i ~= nil and j ~= nil then
+				local f = t.viewSprite[i] .. j
+				t.spr[f] = love.graphics.newImage(name .. t.viewSprite[i] .. j .. ".png")
+				print(name..t.viewSprite[i]..j..".png")
+			elseif i == nil and j == nil then
+				t.spr[1] = love.graphics.newImage(name .. ".png") 
+				print(name..".png")
+			elseif i == nil and j ~= nil then
+				t.spr[j] = love.graphics.newImage(name .. j .. ".png")
+				print(name .. j .. ".png")
+			end
 		end
 	end
-	return t2
-end--]]
+    return t
+end
 
 function setSprAnim(t, anim)
     t.curAnim = anim
@@ -60,36 +51,19 @@ function updateSpr(t, dt)
         t.curDelay = t.frameDelay
         t.curFrame = t.curFrame + 1
         -- если фрэймбольше 4 то делаем снова 1
-        if t.curFrame > #framesOrderMobs then
-            t.curFrame = 1
-        end
-    end
-end
-
-function updateSpr2(t, dt)
-    -- ждем delay и увеличиваем номер фрейма в анимации
-    t.curDelay = t.curDelay - dt
-    if t.curDelay < 0 then
-        t.curDelay = t.frameDelay
-        t.curFrame = t.curFrame + 1
-        -- если фрэймбольше 4 то делаем снова 1
-        if t.curFrame > #framesOrderBurn then
+        if t.curFrame > #t.framesOrder then
             t.curFrame = 1
         end
     end
 end
 
 function drawSpr(t, x, y)
-    --	print(t.curAnim..t.curFrame)
-    love.graphics.draw(t.spr[t.curAnim .. framesOrderMobs[t.curFrame]], x - t.width / 2, y - t.height / 2)
-end
-
-function drawSpr2(t, x, y)
-    --print(t.curAnim..t.curFrame)
-    love.graphics.draw(t.spr[t.curAnim .. framesOrderBurn[t.curFrame]], x - t.width / 2, y - t.height / 2)
-end
-
-function drawSpr0(t, x, y)
-    --print(t.curAnim..t.curFrame)
-    love.graphics.draw(t.spr[t.curAnim .. framesOrderItem[t.curFrame]], x - t.width / 2, y - t.height / 2)
+--	print(t.name.."  "..t.curAnim..t.curFrame)
+	if #t.framesOrder == 1 then
+		love.graphics.draw(t.spr[#t.spr], x - t.width / 2, y - t.height / 2)
+	elseif #t.framesOrder == 2 then
+	    love.graphics.draw(t.spr[t.framesOrder[t.curFrame]], x - t.width / 2, y - t.height / 2)
+	else
+		love.graphics.draw(t.spr[t.curAnim .. t.framesOrder[t.curFrame]], x - t.width / 2, y - t.height / 2)
+	end
 end
