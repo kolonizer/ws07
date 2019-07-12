314b27d431c92c3a9ff9a908f533519e24f15411
DimaKorytov <d.a.korytov@gmail.com>
2019-07-12 11:17:46 +0700

    sprite

--- project/monster.lua
+++ project/monster.lua
@@ -17,7 +17,7 @@ function CreateMonster(id, tip)
     end
     if tip == 'Slime' then
         Objects[#Objects + 1] = { id = id, cellX = id % n, cellY = math.floor(id / n) + 1,
-		name = "Slime", Type = "circle", mode = "line", sprite = newSpr("spr/enemyUnknown", 51, 51, 23, 1, 1),
+		name = "Slime", Type = "circle", mode = "line", sprite = newSpr("spr/modelSlime", 51, 51, 0.5, 1, 2),
 		x = collide.XYFromID(id)[1] * size + e_x, y = (collide.XYFromID(id)[2] + 2) * size + e_y,
 		radius = 30, colour = { 255, 255, 255, 0 }, HP = 1, Def = 1, speed = 50,lastTime=0,
 		hit={cd=2,radius=40,x=0,y=0,Type="circle",damage=1}}
@@ -31,28 +31,28 @@ function CreateMonster(id, tip)
     end
     if tip == 'Ghost' then
         Objects[#Objects + 1] = { id = id, cellX = id % n, cellY = math.floor(id / n) + 1,
-		name = "Ghost", Type = "circle", mode = "line", sprite = newSpr("spr/enemyUnknown", 51, 51, 23, 1, 1),
+		name = "Ghost", Type = "circle", mode = "line", sprite = newSpr("spr/modelGost", 51, 51, 0.8, 1, 2),
 		x = collide.XYFromID(id)[1] * size + e_x, y = (collide.XYFromID(id)[2] + 2) * size + e_y,
 		radius = 15, colour = { 255, 255, 255, 0 }, HP = 1, Def = 1, speed = 100,lastTime=0,
 		hit={cd=2,radius=25,x=0,y=0,Type="circle",damage=1}}
     end
     if tip == 'Snake' then
         Objects[#Objects + 1] = { id = id, cellX = id % n, cellY = math.floor(id / n) + 1,
-		name = "Snake", Type = "circle", mode = "line", sprite = newSpr("spr/enemyUnknown", 51, 51, 23, 1, 1),
+		name = "Snake", Type = "circle", mode = "line", sprite = newSpr("spr/modelSnake", 51, 51, 0.7, 1, 2),
 		x = collide.XYFromID(id)[1] * size + e_x, y = (collide.XYFromID(id)[2] + 2) * size + e_y,
 		radius = 15, colour = { 255, 255, 255, 0 }, HP = 1, Def = 1, speed = 10,lastTime=0,
 		hit={cd=2,radius=25,x=0,y=0,Type="circle",damage=1}}
     end
     if tip == 'Wolf' then
         Objects[#Objects + 1] = { id = id, cellX = id % n, cellY = math.floor(id / n) + 1,
-		name = "Wolf", Type = "circle", mode = "line", sprite = newSpr("spr/enemyUnknown", 51, 51, 23, 1, 1),
+		name = "Wolf", Type = "circle", mode = "line", sprite = newSpr("spr/modelWolf", 51, 51, 0.51, 1, 2),
 		x = collide.XYFromID(id)[1] * size + e_x, y = (collide.XYFromID(id)[2] + 2) * size + e_y,
 		radius = 5, colour = { 255, 255, 255, 0 }, HP = 1,Def=1, speed = 290, active = false,lastTime=0,
 		hit={cd=2,radius=15,x=0,y=0,Type="circle",damage=1}}
     end
     if tip == 'Mushroom' then
         Objects[#Objects + 1] = { id = id, cellX = id % n, cellY = math.floor(id / n) + 1,
-		name = "Mushroom", Type = "circle", mode = "line", sprite = newSpr("spr/enemyUnknown", 51, 51, 23, 1, 1),
+		name = "Mushroom", Type = "circle", mode = "line", sprite = newSpr("spr/modelMushroom", 51, 51, 0.7, 1, 2),
 		x = collide.XYFromID(id)[1] * size + e_x, y = (collide.XYFromID(id)[2] + 2) * size + e_y,
 		radius = 5, colour = { 255, 255, 255, 0 }, HP = 1, Def = 1, speed = 0, lastTime=0,
 		hit={cd=2,radius=15,x=0,y=0,Type="circle",damage=1}}
